162cb2531508c4c645bc5a09fb606cb8d429e562
yurilamijo <yurilamijo@hotmail.com>
2022-03-04 15:34:42 +0100

    Added IdentityServer 4

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -3,6 +3,7 @@ using GenericRepository.Settings;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson;
+using Identity.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,7 @@ BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 
 var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+var identityServerSettings = configuration.GetSection(nameof(IdentityServerSettings)).Get<IdentityServerSettings>();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>()
     .AddRoles<ApplicationRole>()
@@ -20,6 +22,18 @@ builder.Services.AddDefaultIdentity<ApplicationUser>()
         mongoDbSettings.ConnectionString,
         serviceSettings.ServiceName
     );
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseSuccessEvents = true;
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseFailureEvents = true;
+})
+    .AddAspNetIdentity<ApplicationUser>()
+    .AddInMemoryApiScopes(identityServerSettings.ApiScopes)
+    .AddInMemoryClients(identityServerSettings.Clients)
+    .AddInMemoryIdentityResources(identityServerSettings.IdentityResources)
+    .AddDeveloperSigningCredential();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -41,6 +55,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles(); // Check
 
 app.UseAuthentication();
+
+app.UseIdentityServer();
+
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ Identity/Settings/IdentityServerSettings.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer4.Models;
+
+namespace Identity.Settings
+{
+    public class IdentityServerSettings
+    {
+        public IReadOnlyCollection<ApiScope> ApiScopes { get; set; } = Array.Empty<ApiScope>();
+
+        public IReadOnlyCollection<Client> Clients { get; init; }
+
+        public IReadOnlyCollection<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+    }
+}
