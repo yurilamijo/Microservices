2d8828180bffe93487870c5d5f7fe41e5427a555
yurilamijo <yurilamijo@hotmail.com>
2022-03-20 16:01:21 +0100

    New front-end

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -10,11 +10,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 var configuration = builder.Configuration;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var MyAllowSpecificOriginsSetting = "AllowedOrigin";
+
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 
 var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 var identityServerSettings = configuration.GetSection(nameof(IdentityServerSettings)).Get<IdentityServerSettings>();
+
+// Configure CORS
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policyBuilder => {
+            policyBuilder.WithOrigins(configuration[MyAllowSpecificOriginsSetting])
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 builder.Services.Configure<IdentitySettings>(configuration.GetSection(nameof(IdentitySettings)))
     .AddDefaultIdentity<ApplicationUser>()
@@ -56,6 +69,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(MyAllowSpecificOrigins);
 }
 
 app.UseHttpsRedirection();
